8977ddf3dc31bafef8a51d8346abfb93025e7354
박소희 <shpark@daimresearch.com>
2025-12-30 18:28:21 +0900

    updateInfo로 port관련 정보 관리

---
+++ Robot1/PortInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Robot1;
+
+public record PortInfo
+{
+    public required string PortId { get; init; }
+
+    public Canvas? Carrier { get; set; }
+
+    public string? CarrierId { get; set; }
+}
